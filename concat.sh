a649996b2e7dc1c686e69629c2cbbd09feda22b3
Vlas_Mastykash <mastykash.vlas@gmail.com>
2021-11-11 05:39:33 +0600

    README.md concatination

--- concat.sh
+++ concat.sh
@@ -4,5 +4,5 @@ git pull
 cd /f/courses/github_repos/v-las/
 git pull
 cat /f/courses/github_repos/v-las/title.md /f/courses/github_repos/pdf-presentations/README.md /f/courses/github_repos/v-las/ending.md > README.md
-git commit -am 'Update README.md'
+git commit -am 'README.md concatination'
 git push
